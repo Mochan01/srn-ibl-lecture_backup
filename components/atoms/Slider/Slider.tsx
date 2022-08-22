0ecefdd26d52431df91f590942c073a15e0725ff
Mochan01 <ryohei.s@fantamstick.jp>
2022-08-22 21:12:59 +0900

    progress buttons were added

--- components/atoms/Slider/Slider.tsx
+++ components/atoms/Slider/Slider.tsx
@@ -1,7 +1,8 @@
-import React, { FC, ReactElement } from "react";
+import React, { FC, ReactElement, useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import { ArrowBtn } from "../ArrowBtn/ArrowBtn";
 
 export interface SliderProps {
   slides: ReactElement[];
@@ -10,19 +11,28 @@ export interface SliderProps {
 export const Slider: FC<SliderProps> = ({
   slides
 }) => {
+
+  const idPrev = "button_prev";
+  const idNext = "button_next";
+
   return (
     <>
       <Swiper
-        navigation={ true }
         pagination={ true }
         mousewheel={ true } // デバッグ用
         allowTouchMove={ false }
         speed={ 1 } // スライドエフェクトを止める
         modules={ [Navigation, Pagination, Mousewheel, Keyboard] }
         className="mySwiper"
+        navigation={ {
+          prevEl: `#${ idPrev }`,
+          nextEl: `#${ idNext }`
+        } }
       >
         { slides.map((x, i) => <SwiperSlide key={ i }>{ x }</SwiperSlide>) }
       </Swiper>
+      <ArrowBtn id={ idPrev } dir="prev" />
+      <ArrowBtn id={ idNext } dir="next" />
     </>
   );
 };
