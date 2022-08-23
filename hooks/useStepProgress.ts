cf21663ec7471d6bef6582738e6c29fced9b9f93
Mochan01 <ryohei.s@fantamstick.jp>
2022-08-23 23:26:03 +0900

    make hooks use useReducer instead of useState

--- hooks/useStepProgress.ts
+++ hooks/useStepProgress.ts
@@ -1,27 +1,36 @@
-import { useState, useEffect } from "react";
-import { SlideProps } from "../components/molecules/Slide/Slide";
+import { useReducer, useEffect } from "react";
+
+type StepProgressState = number;
+type StepProgressAction = "ArrowRight" | "ArrowLeft";
 
 export const useStepProgress = (len: number) => {
 
+  const reducer = (
+    state: StepProgressState,
+    action: StepProgressAction
+  ): StepProgressState => {
+    switch(action) {
+      case "ArrowRight":
+        return state >= len ? state : state + 1;
+      default:
+        return state <= 1 ? 1 : state - 1;
+    }
+  };
+
   // 1はじまり
-  const [step, setStep] = useState(1);
+  const initialState: StepProgressState = 1;
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
 
     // For debugging.
-    window.addEventListener("keydown", ({ key }) => {
-
-      setStep(state => {
-
-        if (key === "ArrowRight") {
-          return state >= len ? state : state + 1;
-        }
-
-        return state <= 1 ? 1 : state - 1;
-      });
-
+    window.addEventListener("keydown", (e) => {
+      const key = e.key as StepProgressAction;
+      dispatch(key);
     });
+
   }, []);
 
-  return step;
+  return state;
 };
