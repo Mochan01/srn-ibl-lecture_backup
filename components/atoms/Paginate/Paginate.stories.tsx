351eda8fa758a6f0778ba57312ebf1617236d4bf
Mochan01 <ryohei.s@fantamstick.jp>
2022-08-31 13:16:52 +0900

    make paginate to visible on storybook

--- components/atoms/Paginate/Paginate.stories.tsx
+++ components/atoms/Paginate/Paginate.stories.tsx
@@ -1,16 +1,50 @@
 import React from "react";
 // also exported from "@storybook/react" if you can deal with breaking changes in 6.1
 import { Story, Meta } from "@storybook/react/types-6-0";
-
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
 import { Paginate, PaginateProps } from "./Pagenate";
+import { classNames } from "../../../data/ClassNames";
 
 export default {
   title: "atoms/Paginate",
   component: Paginate
 } as Meta;
 
-const Template: Story<PaginateProps> = (args) => <Paginate {...args} />;
+const Template: Story<PaginateProps> = (args) => {
+
+
+
+  return <>
+    <Swiper
+      allowTouchMove={ false }
+      speed={ 1 } // スライドエフェクトを止める
+      modules={ [Navigation, Pagination, Mousewheel, Keyboard] }
+      className="mySwiper"
+      navigation={ {
+        prevEl: `#${ classNames.arrowPrev }`,
+        nextEl: `#${ classNames.arrowNext }`
+      } }
+      pagination={ {
+        el: `#${ classNames.paginate }`,
+        clickable: true
+      } }
+    >
+      <SwiperSlide />
+      <SwiperSlide />
+      <SwiperSlide />
+      <SwiperSlide />
+      <SwiperSlide />
+      <SwiperSlide />
+      <SwiperSlide />
+      <SwiperSlide />
+    </Swiper>
+    <Paginate {...args} />
+  </>;
+};
 
 export const Sample: { args: PaginateProps } = Template.bind({});
 Sample.args = {
+  id: classNames.paginate
 };
