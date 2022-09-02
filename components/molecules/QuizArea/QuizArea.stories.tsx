9c8dbe09ce246f6a9e377c7b303cbeb8b6df1a67
Mochan01 <ryohei.s@fantamstick.jp>
2022-09-02 16:59:50 +0900

    mock data was updated

--- components/molecules/QuizArea/QuizArea.stories.tsx
+++ components/molecules/QuizArea/QuizArea.stories.tsx
@@ -2,6 +2,7 @@ import React from "react";
 // also exported from "@storybook/react" if you can deal with breaking changes in 6.1
 import { Story, Meta } from "@storybook/react/types-6-0";
 import { QuizArea, QuizAreaProps } from "./QuizArea";
+import { questions } from "../../../data/mock";
 
 export default {
   title: "molecules/QuizArea",
@@ -12,21 +13,12 @@ const Template: Story<QuizAreaProps> = (args) => <QuizArea {...args} />;
 
 export const Three: { args: QuizAreaProps } = Template.bind({});
 Three.args = {
-  questions: [
-    "アリスは川辺でおねえさんのよこにすわって",
-    "なんにもすることがないのでとても退屈",
-    "一、二回はおねえさんの読んでいる本をのぞいてみたけれど、そこには絵も会話もないのです。"
-  ],
+  questions,
   correctIndex: 0
 };
 
 export const Four: { args: QuizAreaProps } = Template.bind({});
 Four.args = {
-  questions: [
-    "アリスは川辺でおねえさんのよこにすわって",
-    "なんにもすることがないのでとても退屈",
-    "一、二回はおねえさんの読んでいる本をのぞいてみたけれど、そこには絵も会話もないのです。",
-    "「絵や会話のない本なんて、なんの役にもたたないじゃないの」とアリスは"
-  ],
+  questions,
   correctIndex: 0
 };
--- data/mock.ts
+++ data/mock.ts
@@ -2,18 +2,31 @@ import { StepDataProps } from "../components/organisms/Slide/Slide";
 
 export const sound = "sample.mp3";
 
+export const questions = [
+  "アリスは川辺でおねえさんのよこにすわって",
+  "なんにもすることがないのでとても退屈",
+  "一、二回はおねえさんの読んでいる本をのぞいてみたけれど、そこには絵も会話もないのです。",
+  "「絵や会話のない本なんて、なんの役にもたたないじゃないの」とアリスは"
+];
+
 export const stepA: StepDataProps = {
   image: "sample_a.png",
+  questions,
+  correctIndex: 0,
   sound
 };
 
 export const stepB: StepDataProps = {
   image: "sample_b.png",
+  questions,
+  correctIndex: 0,
   sound
 };
 
 export const stepC: StepDataProps = {
   image: "sample_c.png",
+  questions,
+  correctIndex: 0,
   sound
 };
 
