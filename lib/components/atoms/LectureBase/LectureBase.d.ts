372da6c4014041701390f83c657e5ccc119e7eac
Mochan01 <ryohei.s@fantamstick.jp>
2022-09-06 19:00:09 +0900

    build

---
+++ lib/components/atoms/LectureBase/LectureBase.d.ts
@@ -0,0 +1,5 @@
+import { FC } from "react";
+export interface LectureBaseProps {
+    onClick?: () => void;
+}
+export declare const LectureBase: FC<LectureBaseProps>;
--- lib/data/SIZE.d.ts
+++ lib/data/SIZE.d.ts
@@ -1,6 +1,7 @@
 export declare const SIZE: {
     readonly W: 1000;
     readonly H: 692;
+    readonly FRAME_H: 750;
     readonly PANEL_A_W: 260;
     readonly PANEL_A_H: 74;
     readonly PANEL_B_W: 480;
