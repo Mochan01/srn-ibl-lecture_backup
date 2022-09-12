b37e1910151765914b76ea704697610f312c8d72
Mochan01 <ryohei.s@fantamstick.jp>
2022-09-12 16:52:56 +0900

    build

---
+++ lib/components/atoms/Teacher/Teacher.d.ts
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { AnimationType } from "src-ibl-lecture-master/variable_types/StepType";
+export interface TeacherProps {
+    animation: AnimationType;
+}
+export declare const Teacher: FC<TeacherProps>;
