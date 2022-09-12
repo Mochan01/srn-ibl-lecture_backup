c0bde9637d7929794b87c2a64f550b25b33f1a75
Mochan01 <ryohei.s@fantamstick.jp>
2022-09-12 18:11:55 +0900

    add Bubble

---
+++ components/atoms/Bubble/Bubble.tsx
@@ -0,0 +1,45 @@
+import React, { FC, ReactElement } from "react";
+import styled from "styled-components"
+const speech_Bubble
+  = new URL("../../../assets/speech_bubble.png", import.meta.url).toString();
+
+export interface BubbleProps {
+  children?: ReactElement;
+}
+
+interface MainProps {
+}
+
+const Main = styled.div.attrs<MainProps>(
+  (props) => ({
+    style: {
+    }
+  })
+)<MainProps>`
+  background-image: url(${ speech_Bubble });
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 185px;
+  height: 127px;
+  padding: 22px 22px 40px 22px;
+
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  opacity: .5;
+  overflow: hidden;
+  line-height: 1.4;
+`;
+
+export const Bubble: FC<BubbleProps> = ({
+  children
+}) => {
+  return (
+    <Main>
+      <Wrapper>
+        { children }
+      </Wrapper>
+    </Main>
+  );
+};
