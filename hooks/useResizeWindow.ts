0206dfe0e0aa4595453eb53191c9e1449aed2a34
Mochan01 <ryohei.s@fantamstick.jp>
2022-09-16 19:01:24 +0900

    fix for next js

--- hooks/useResizeWindow.ts
+++ hooks/useResizeWindow.ts
@@ -2,14 +2,15 @@ import { useEffect, useState } from "react";
 
 export const useResizeWindow = (): [number, number] => {
 
-  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
-  const [windowHeight, setWindowHeight] = useState<number>(window.innerHeight);
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(0);
 
   useEffect(() => {
     const setWindowSize = () => {
       setWindowWidth(window.innerWidth);
       setWindowHeight(window.innerHeight);
     };
+    setWindowSize();
     window.addEventListener("resize", setWindowSize);
     return () => window.removeEventListener("resize", setWindowSize);
   }, []);
