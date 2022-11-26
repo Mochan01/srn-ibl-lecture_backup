e40eadb7c1eb622f57e0f2e118a52106b3d52480
Mochan01 <ryohei.s@fantamstick.jp>
2022-11-26 18:06:34 +0900

    Fix the type of src/utils/handleStepArray.ts

--- src/utils/handleStepArray.ts
+++ src/utils/handleStepArray.ts
@@ -1,5 +1,4 @@
 import { StepType } from "src-ibl-lecture-master/types/StepType";
-import _ from "lodash";
 
 type Callback<T> = (data: StepType[]) => T;
 
@@ -10,33 +9,9 @@ type Callback<T> = (data: StepType[]) => T;
  * @returns
  */
 export const handleStepArray =
-  (data: StepType | StepType[]) =>
-  <T>(callback: Callback<T>): T => {
-    if (!Array.isArray(data)) return;
-    return callback(data);
-  };
-
-/**
- * そのスライドのステップ画像をすべて返す
- * @param data
- * @returns
- */
-export const getPics: (steps: StepType[]) => string[] = (data: StepType[]) => {
-  const result = _.compact(data.map((x) => x.image.display_file));
-  return result.length ? result : void 0;
-};
-
-/**
- * そのスライドのクイズをすべて返す
- * @param data
- * @returns
- */
-export const getQuestions: (steps: StepType[]) => Array<StepType["question"]> =
-  (data: StepType[]) => {
-    let result = data.map((x) => x.question);
-    result = result.filter((x) => x.button_1); // button_1がないなら回答ステップではないことにする
-    return result.length ? result : void 0;
-  };
+  (data: StepType[]) =>
+  <T>(callback: Callback<T>): T =>
+    callback(data);
 
 export const getLength: (steps: StepType[]) => number = (data: StepType[]) => {
   return data.length;
