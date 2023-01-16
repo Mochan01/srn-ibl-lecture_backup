6a3e177edd04bf9f57ee99252f7b7f04a4898d9e
Mochan01 <ryohei.s@fantamstick.jp>
2023-01-16 19:16:56 +0900

    Append the feature of Quiz

---
+++ src/features/Quiz/Quiz.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+export interface QuizProps {}
+
+const Main = styled.div`
+`;
+
+export const Quiz: FC<QuizProps> = (props) => {
+  return (
+    <Main />
+  );
+};
