833508a7357697c260d70e765f518c1443560c01
aks-tamura <tamura-junpei@akiba.kayac.studio>
2023-01-23 21:20:06 +0900

    三角形を外に出すように修正、要素を中央にするように修正

--- src/elements/PartsSelectSlider/index.tsx
+++ src/elements/PartsSelectSlider/index.tsx
@@ -1,13 +1,12 @@
 import React, { FC, useState } from "react";
 import { Swiper } from "swiper/react";
 import { SwiperSlide } from "swiper/react";
-import { type Swiper as SwiperRef } from "swiper";
+import type { Swiper as SwiperRef } from "swiper";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css";
 import styled from "styled-components";
-
 
 // TODO:画面実装時に変更する
 export const buttonColor = "orange" as const;
@@ -21,49 +20,42 @@ export interface PartsSelectSliderProps {
   selectIndex: number;
   className?: string;
 }
-export const Main = styled(Swiper)`
+
+// TODO:画面実装時にwidthやheightの値を修正する
+const Main = styled.div`
+  width: 1200px;
+  height: 500px;
+  padding: 20px;
+`;
+
+const SSwiperContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SSwiper = styled(Swiper)`
   .swiper-wrapper {
-    width: 100%;
+    width: 938px;
     height: 100%;
-    padding-top: 8px;
-    padding-bottom: 8px;
+    padding-top: 16px;
+    padding-bottom: 16px;
   }
 
   .swiper-slide img {
     display: block;
-    width: 200px;
-    height: 200px;
+    width: 100%;
+    height: 100%;
     object-fit: cover;
   }
-  .swiper-button-next {
-    top:47%;
-    border-top: 30px solid transparent;
-    border-bottom: 30px solid transparent;
-    border-left: 30px solid ${buttonColor};
-  }
-  .swiper-button-next:after{
-    content: ''
-  }
-  .swiper-button-prev {
-    top:47%;
-    border-top: 30px solid transparent;
-    border-bottom: 30px solid transparent;
-    border-right: 30px solid ${buttonColor};
-  }
-  .swiper-button-prev:after{
-    content: ''
-  }
-  `;
-
-const SImage = styled.img`
 `;
+
+const SImage = styled.img``;
 
 // 下に表示するページネーションの棒
 const SBar = styled.div`
   margin-top: 16px;
   margin-left: auto;
   margin-right: auto;
-  text-align: center;
   width: 200px;
   height: 3px;
   background-color: gray;
@@ -71,69 +63,98 @@ const SBar = styled.div`
 `;
 
 const SCircle = styled.div`
+  background-color: gray;
   width: 10px;
   height: 10px;
-  border-radius: 50%; 
-  position: absolute; 
+  border-radius: 50%;
+  position: absolute;
   top: -3px;
 `;
 
 // 選択した画像のindexによって、●を移動する
-const SSelectCircle = styled(SCircle).attrs<{left: number }>(({left}) => ({
-  style: { left: `${left}%`,},
-})
-)<{left: number}>`
+const SSelectCircle = styled(SCircle).attrs<{ left: number }>(({ left }) => ({
+  style: { left: `${left}%` },
+}))<{ left: number }>`
   top: -6px;
   width: 15px;
   height: 15px;
-  background-color: ${buttonColor}; 
+  background-color: ${buttonColor};
 `;
 
 const SLeftCircle = styled(SCircle)`
-  background-color: gray; 
-  left:0;
+  left: 0;
 `;
 const SRightCircle = styled(SCircle)`
-  background-color: gray; 
-  left:100%;
+  left: 100%;
 `;
-
+const SSwiperPrevButton = styled.div`
+  width: 50px;
+  height: 50px;
+  border-top: 30px solid transparent;
+  border-bottom: 30px solid transparent;
+  border-right: 30px solid ${buttonColor};
+`;
+const SSwiperNextButton = styled.div`
+  width: 50px;
+  height: 50px;
+  border-top: 30px solid transparent;
+  border-bottom: 30px solid transparent;
+  border-left: 30px solid ${buttonColor};
+`;
 /**
  * 特別レクチャー(衛生組み立て画面）のパーツセレクト部分のスライダー
  */
-export const PartsSelectSlider: FC<PartsSelectSliderProps> = ({selectIndex, images, onSelect}) => {
+export const PartsSelectSlider: FC<PartsSelectSliderProps> = ({
+  selectIndex,
+  images,
+  onSelect,
+}) => {
   const [swiperObject, onSwiper] = useState<SwiperRef>();
   const [activeIndex, setActiveIndex] = useState(0);
-  const onSlideChange = (swiper: SwiperRef) =>{
+  const onSlideChange = (swiper: SwiperRef) => {
     onSelect(swiper.realIndex);
     setActiveIndex(swiper.realIndex);
   };
 
   return (
-    <div css={"width: 928px"}>
-    <Main 
-     modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={spaceBetween}
-      slidesPerView={4}
-      initialSlide={selectIndex}
-      loop={true}
-      loopedSlides={images.length}
-      navigation={true}
-      scrollbar={{ draggable: true }}
-      onSwiper={onSwiper}
-      onSlideChange={onSlideChange}
-      speed={duration}
-    >
-      {images.map((image, i) => 
-      <SwiperSlide key={i}><SImage src={image} css={activeIndex === i ? `border: solid 5px ${isActiveColor}`: ""}/></SwiperSlide>
-      )}
+    <Main>
+      <SSwiperContainer>
+        <SSwiperPrevButton id="button_prev" />
+        <SSwiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={spaceBetween}
+          slidesPerView={4}
+          initialSlide={selectIndex}
+          loop={true}
+          loopedSlides={images.length}
+          navigation={{
+            // パラメータを設定
+            prevEl: "#button_prev",
+            nextEl: "#button_next",
+          }}
+          scrollbar={{ draggable: true }}
+          onSwiper={onSwiper}
+          onSlideChange={onSlideChange}
+          speed={duration}
+        >
+          {images.map((image, i) => (
+            <SwiperSlide
+              key={i}
+              css={
+                activeIndex === i ? `border: solid 4px ${isActiveColor}` : ""
+              }
+            >
+              <SImage src={image} />
+            </SwiperSlide>
+          ))}
+        </SSwiper>
+        <SSwiperNextButton id="button_next" />
+      </SSwiperContainer>
+      <SBar>
+        <SLeftCircle />
+        <SRightCircle />
+        <SSelectCircle left={(100 / (images.length - 1)) * activeIndex} />
+      </SBar>
     </Main>
-    <SBar>
-      <SLeftCircle/>
-      <SRightCircle/>
-      <SSelectCircle left={(100 / (images.length -1)) * activeIndex}/>
-    </SBar>
-    </div>
   );
 };
-
