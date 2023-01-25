52688c2edb95d369d9b05c4c54285079f2ee4659
aks-tamura <tamura-junpei@akiba.kayac.studio>
2023-01-25 20:26:53 +0900

    コンポーネント実装

--- src/elements/QuizInput/components/TextBox.tsx
+++ src/elements/QuizInput/components/TextBox.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import styled from "styled-components";
 
 export interface TextBoxProps {
@@ -6,14 +6,47 @@ export interface TextBoxProps {
   onEnter?: () => void;
 }
 
-const Main = styled.input``;
+const STitle = styled.div`
+  font-size: 18px;
+  color: #5a5a5a;
+  margin-bottom: 12px;
+`;
+const STextBox = styled.input`
+  height: 64px;
+  width: 100%;
+  border-radius: 10px;
+  font-size: 23px;
+  color: #5a5a5a;
+  padding-left: 10px;
+  border: solid 1px #5a5a5a;
+  background-color: #ffffff;
+  &::placeholder {
+    color: #a9a9a9;
+  }
+  :hover {
+    outline: solid 2px #1a6cf1;
+    border: none;
+  }
+  :focus {
+    outline: solid 2px #1a6cf1;
+    border: none;
+  }
+`;
 
 export const TextBox: FC<TextBoxProps> = ({ onChange, onEnter }) => {
+  const [isHover, setIsHover] = useState(false);
+
   return (
-    <Main
-      type="text"
-      onChange={(e) => onChange && onChange(e.currentTarget.value)}
-      onKeyDown={(e) => onEnter && e.key === "Enter" && onEnter()}
-    />
+    <>
+      <STitle css={isHover ? "color: #1a6cf1" : ""}>あなたの解答</STitle>
+      <STextBox
+        type="text"
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+        placeholder="ここに答えを入力"
+        onChange={(e) => onChange && onChange(e.currentTarget.value)}
+        onKeyDown={(e) => onEnter && e.key === "Enter" && onEnter()}
+      />
+    </>
   );
 };
