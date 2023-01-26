5a05397e67203009fa3a6dce60d095c982ca3e1a
aks-tamura <tamura-junpei@akiba.kayac.studio>
2023-01-26 12:20:27 +0900

    コンポーネント実装

---
+++ src/elements/PartsSelectBtn/index.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+const ImgSelect = new URL(
+  "../../assets/prod/button_select.png",
+  import.meta.url
+).toString();
+const ImgSelected = new URL(
+  "../../assets/prod/button_selected.png",
+  import.meta.url
+).toString();
+
+const Main = styled.div<Pick<PartsSelectBtnProps, "isSelected">>(
+  ({ isSelected }) => {
+    return `
+    background-image: ${
+      isSelected ? `url(${ImgSelected})` : `url(${ImgSelect})`
+    };
+    background-repeat: no-repeat;
+    background-size: contain;
+    width: 100px;
+    height: 40px;
+`;
+  }
+);
+
+export interface PartsSelectBtnProps {
+  onClick: () => void;
+  isSelected: boolean;
+  className?: string;
+}
+
+/**
+ * パーツセレクト部分のボタン
+ */
+export const PartsSelectBtn: FC<PartsSelectBtnProps> = ({
+  onClick,
+  isSelected,
+}) => {
+  return <Main {...{ isSelected, onClick }} />;
+};
