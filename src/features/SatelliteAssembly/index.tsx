b6d3672d80bb9208b99edf0ee36d7ee8bbeefb61
aks-tamura <tamura-junpei@akiba.kayac.studio>
2023-01-27 17:05:47 +0900

    仮レイアウト実装

--- src/features/SatelliteAssembly/index.tsx
+++ src/features/SatelliteAssembly/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useMemo } from "react";
+import styled from "styled-components";
 import { LectureFrame } from "../../elements/LectureFrame";
 import { MasterData } from "./types";
 import {
@@ -9,6 +10,35 @@ import {
   getRocketIDs,
 } from "./utils";
 
+// height: ${SIZE.H};
+// width: ${SIZE.W};
+const Main = styled.div`
+  height: 890px;
+  width: 1286px;
+  background-color: red;
+  display: flex;
+`;
+
+const PartsViewArea = styled.div`
+  height: 890px;
+  width: 574px;
+  background-color: blue;
+`;
+const ButtonArea = styled.div`
+  height: 59px;
+  width: 680px;
+  background-color: blue;
+`;
+const ParameterArea = styled.div`
+  height: 210px;
+  width: 680px;
+  background-color: blue;
+`;
+const PartDetailArea = styled.div`
+  height: 543px;
+  width: 680px;
+  background-color: blue;
+`;
 export interface SatelliteAssemblyProps {
   /**
    * プレイヤーが選択しているミッションID
@@ -42,8 +72,26 @@ export const SatelliteAssembly: FC<SatelliteAssemblyProps> = ({
   const busIDs = getIDs(getBusIDs); // 積載パーツ
   const rocketIDs = getIDs(getRocketIDs); // 打ち上げロケット
 
+  console.log("missionPartsIDs", missionPartsIDs);
+  console.log(" batteryIDs", batteryIDs);
+  console.log(" busIDs", busIDs);
+  console.log(" rocketIDs", rocketIDs);
+  console.log(" missionData", missionData);
+  console.log(" masterData", masterData);
+
   return (
     <LectureFrame unitName="とりあえず仮" unitTitle="とりあえず仮">
+      <Main>
+        <PartsViewArea></PartsViewArea>
+        <div>
+          <div css={"margin-top: 17px"}></div>
+          <ButtonArea></ButtonArea>
+          <div css={"margin-top: 17px"}></div>
+          <ParameterArea></ParameterArea>
+          <div css={"margin-top: 17px"}></div>
+          <PartDetailArea></PartDetailArea>
+        </div>
+      </Main>
       {/* ここに衛生組み立て画面の実装を書く */}
     </LectureFrame>
   );
