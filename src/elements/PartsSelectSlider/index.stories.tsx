196ef65a3a38469649790d10d0d52e8cf86ae440
aks-tamura <tamura-junpei@akiba.kayac.studio>
2023-01-27 13:17:17 +0900

    名前を表示するように修正、サイズをXDのサイズに修正、選択済みの場合は背景色を明るくするように修正

--- src/elements/PartsSelectSlider/index.stories.tsx
+++ src/elements/PartsSelectSlider/index.stories.tsx
@@ -15,12 +15,19 @@ export const sample = template.bind({});
 sample.args = {
   selectIndex: 0,
   selectedIndexes: [1, 2],
+  items: [
+    { name: "ああああああああ", image: "https://placekitten.com/116/116" },
+    { name: "いいいいいいい", image: "https://placekitten.com/117/116" },
+    { name: "うううううう", image: "https://placekitten.com/118/116" },
+    { name: "えええええ", image: "https://placekitten.com/119/116" },
+    { name: "おおおお", image: "https://placekitten.com/112/116" },
+  ],
   images: [
+    "https://placekitten.com/120/116",
     "https://placekitten.com/116/116",
     "https://placekitten.com/117/116",
     "https://placekitten.com/118/116",
     "https://placekitten.com/119/116",
-    "https://placekitten.com/120/116",
   ],
   onSelect: (i) => console.log(`${i}番目を選択した`),
 };
--- src/elements/PartsSelectSlider/index.tsx
+++ src/elements/PartsSelectSlider/index.tsx
@@ -8,25 +8,19 @@ import "swiper/css/pagination";
 import "swiper/css";
 import styled from "styled-components";
 
-// TODO:画面実装時に変更する
-export const buttonColor = "orange" as const;
-export const isActiveColor = "yellow" as const;
-export const spaceBetween = 32 as const;
-export const duration = 250 as const;
+const buttonColor = "#F98A3B" as const;
+const isActiveColor = "#FFFF00" as const;
+const slidesPerView = 4 as const;
+const spaceBetween = 22.666 as const;
+const border = 3 as const;
+const imageSize = 126 as const;
+const duration = 250 as const;
 
-export interface PartsSelectSliderProps {
-  images: string[];
-  onSelect: (index: number) => void;
-  selectIndex?: number;
-  selectedIndexes: number[];
-  className?: string;
-}
-
-// TODO:画面実装時にwidthやheightの値を修正する
 const Main = styled.div`
-  width: 1200px;
-  height: 500px;
-  padding: 20px;
+  width: 680px;
+  height: 150px;
+  text-align: center;
+  padding: 0 11px;
 `;
 
 const SSwiperContainer = styled.div`
@@ -36,30 +30,38 @@ const SSwiperContainer = styled.div`
 
 const SSwiper = styled(Swiper)`
   .swiper-wrapper {
-    /* (image + spaceBetween + border*2 ) *slidesPerView = width*/
-    /* (200 + 32 + 4*2 ) * 4 = 960px */
-    width: 960px;
-    height: 100%;
-    padding-top: 16px;
-    padding-bottom: 16px;
+    width: ${(imageSize + spaceBetween + border * 2) * slidesPerView -
+    spaceBetween}px;
   }
-
   .swiper-slide img {
-    display: block;
-    width: 200px;
-    height: 200px;
-    object-fit: cover;
+    width: ${imageSize}px;
+    height: ${imageSize}px;
   }
 `;
 
-const SImage = styled.img``;
+const SImage = styled.img<{ selected: boolean }>`
+  background-color: ${({ selected }) => (selected ? "#9bb9c8 " : "#5a7e8f ")};
+`;
 
+const SNameArea = styled.div`
+  width: 132px;
+  height: 24px;
+  background-color: #ffffff;
+  bottom: 0;
+  opacity: 0.6;
+  text-align: center;
+  letter-spacing: 0px;
+  color: #5a5a5a;
+  position: absolute;
+  font-size: 16px;
+  font-family: "UD デジタル 教科書体 N-B";
+  line-height: 24px;
+`;
 // 下に表示するページネーションの棒
 const SBar = styled.div`
-  margin-top: 16px;
   margin-left: auto;
   margin-right: auto;
-  width: 200px;
+  width: 144px;
   height: 3px;
   background-color: gray;
   position: relative;
@@ -78,9 +80,9 @@ const SCircle = styled.div`
 const SSelectCircle = styled(SCircle).attrs<{ left: number }>(({ left }) => ({
   style: { left: `${left}%` },
 }))<{ left: number }>`
-  top: -6px;
-  width: 15px;
-  height: 15px;
+  top: -4px;
+  width: 12px;
+  height: 12px;
   background-color: ${buttonColor};
 `;
 
@@ -91,26 +93,40 @@ const SRightCircle = styled(SCircle)`
   left: 100%;
 `;
 const SSwiperPrevButton = styled.div`
-  width: 50px;
-  height: 50px;
-  border-top: 30px solid transparent;
-  border-bottom: 30px solid transparent;
-  border-right: 30px solid ${buttonColor};
+  width: 18px;
+  height: 40px;
+  border-top: 20px solid transparent;
+  border-bottom: 20px solid transparent;
+  border-right: 18px solid ${buttonColor};
 `;
 const SSwiperNextButton = styled.div`
-  width: 50px;
-  height: 50px;
-  border-top: 30px solid transparent;
-  border-bottom: 30px solid transparent;
-  border-left: 30px solid ${buttonColor};
+  width: 18px;
+  height: 40px;
+  border-top: 20px solid transparent;
+  border-bottom: 20px solid transparent;
+  border-left: 18px solid ${buttonColor};
 `;
+
+export interface SliderItem {
+  name: string;
+  image: string;
+}
+
+export interface PartsSelectSliderProps {
+  items: SliderItem[];
+  onSelect: (index: number) => void;
+  selectIndex?: number;
+  selectedIndexes: number[];
+  className?: string;
+}
+
 /**
  * 特別レクチャー(衛生組み立て画面）のパーツセレクト部分のスライダー
  */
 export const PartsSelectSlider: FC<PartsSelectSliderProps> = ({
   selectIndex = 0,
   selectedIndexes,
-  images,
+  items,
   onSelect,
 }) => {
   const [swiperObject, onSwiper] = useState<SwiperRef>();
@@ -129,10 +145,10 @@ export const PartsSelectSlider: FC<PartsSelectSliderProps> = ({
         <SSwiper
           modules={[Navigation, Pagination, Scrollbar, A11y]}
           spaceBetween={spaceBetween}
-          slidesPerView={4}
+          slidesPerView={slidesPerView}
           initialSlide={selectIndex}
           loop={true}
-          loopedSlides={images.length}
+          loopedSlides={items.length}
           navigation={{
             // パラメータを設定
             prevEl: "#button_prev",
@@ -141,32 +157,28 @@ export const PartsSelectSlider: FC<PartsSelectSliderProps> = ({
           onSwiper={onSwiper}
           speed={duration}
         >
-          {images.map((image, i) => (
-            <SwiperSlide
-              // 選択済みの画像は明るくする
-              // TODO:明るくする度合いは画面実装時に調整する
-              css={selectedIndexes.includes(i) ? "filter: brightness(3);" : ""}
-              key={i}
-              onClick={() => onClick(i)}
-            >
-              {i}
+          {items.map((item, i) => (
+            <SwiperSlide key={item.name} onClick={() => onClick(i)}>
               <SImage
-                src={image}
+                selected={selectedIndexes.includes(i)}
+                src={item.image}
                 css={
                   activeIndex === i
-                    ? `border: solid 4px ${isActiveColor}`
-                    : "padding: 4px"
+                    ? `border: solid ${border}px ${isActiveColor}`
+                    : `padding: ${border}px`
                 }
               />
+              <SNameArea>{item.name}</SNameArea>
             </SwiperSlide>
           ))}
         </SSwiper>
         <SSwiperNextButton id="button_next" />
       </SSwiperContainer>
+      <div css={"margin-top: 8px"} />
       <SBar>
         <SLeftCircle />
         <SRightCircle />
-        <SSelectCircle left={(100 / (images.length - 1)) * activeIndex} />
+        <SSelectCircle left={(100 / (items.length - 1)) * activeIndex} />
       </SBar>
     </Main>
   );
