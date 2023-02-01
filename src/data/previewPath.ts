64634b23e7ed5bb05a0bbaeb76e13290eec7f34d
aks-tamura <tamura-junpei@akiba.kayac.studio>
2023-02-01 12:02:59 +0900

    プレビュー画像追加

---
+++ src/data/previewPath.ts
@@ -0,0 +1,23 @@
+// prettier-ignore
+export const previewPath: { [key in string]: string } = {
+  "1_1": new URL("../assets/prod/preview/preview-1_1.png", import.meta.url).toString(),
+  "1_2": new URL("../assets/prod/preview/preview-1_2.png", import.meta.url).toString(),
+  "1_3": new URL("../assets/prod/preview/preview-1_3.png", import.meta.url).toString(),
+  "1_4": new URL("../assets/prod/preview/preview-1_4.png", import.meta.url).toString(),
+  "1_5": new URL("../assets/prod/preview/preview-1_5.png", import.meta.url).toString(),
+  "1_6": new URL("../assets/prod/preview/preview-1_6.png", import.meta.url).toString(),
+  "1_7": new URL("../assets/prod/preview/preview-1_7.png", import.meta.url).toString(),
+  "2_1": new URL("../assets/prod/preview/preview-2_1.png", import.meta.url).toString(),
+  "2_2": new URL("../assets/prod/preview/preview-2_2.png", import.meta.url).toString(),
+  "2_3": new URL("../assets/prod/preview/preview-2_3.png", import.meta.url).toString(),
+  "2_4": new URL("../assets/prod/preview/preview-2_4.png", import.meta.url).toString(),
+  "2_5": new URL("../assets/prod/preview/preview-2_5.png", import.meta.url).toString(),
+  "2_6": new URL("../assets/prod/preview/preview-2_6.png", import.meta.url).toString(),
+  "2_7": new URL("../assets/prod/preview/preview-2_7.png", import.meta.url).toString(),
+  "2_8": new URL("../assets/prod/preview/preview-2_8.png", import.meta.url).toString(),
+  "3_1": new URL("../assets/prod/preview/preview-3_1.png", import.meta.url).toString(),
+  "3_2": new URL("../assets/prod/preview/preview-3_2.png", import.meta.url).toString(),
+  "3_3": new URL("../assets/prod/preview/preview-3_3.png", import.meta.url).toString(),
+  "3_4": new URL("../assets/prod/preview/preview-3_4.png", import.meta.url).toString(),
+  "3_5": new URL("../assets/prod/preview/preview-3_5.png", import.meta.url).toString(),
+};
