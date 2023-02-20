41b1e46ca835f59426f526a212e6b1ae35dd5b46
Mochan01 <ryohei.s@fantamstick.jp>
2023-02-20 17:23:40 +0900

    Append new assets

--- src/data/navigationButtonsPath.ts
+++ src/data/navigationButtonsPath.ts
@@ -1,5 +1,10 @@
 // prettier-ignore
 export const navigationButtonsPath: { [key in string]: string } = {
+  "L1U5_button_01.png": new URL("../assets/navigationButtons/L1U5_button_01.png", import.meta.url).toString(),
+  "L1U5_button_02.png": new URL("../assets/navigationButtons/L1U5_button_02.png", import.meta.url).toString(),
+  "L1U5_button_03.png": new URL("../assets/navigationButtons/L1U5_button_03.png", import.meta.url).toString(),
+  "L1U5_button_04.png": new URL("../assets/navigationButtons/L1U5_button_04.png", import.meta.url).toString(),
+  "L1U5_button_05.png": new URL("../assets/navigationButtons/L1U5_button_05.png", import.meta.url).toString(),
   "L1U6_button_01.png": new URL("../assets/navigationButtons/L1U6_button_01.png", import.meta.url).toString(),
   "L1U6_button_02.png": new URL("../assets/navigationButtons/L1U6_button_02.png", import.meta.url).toString(),
   "L1U6_button_03.png": new URL("../assets/navigationButtons/L1U6_button_03.png", import.meta.url).toString(),
