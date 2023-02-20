dbce3d4dd46d68d7201220ed5c3189d91694c479
Mochan01 <ryohei.s@fantamstick.jp>
2023-02-21 00:12:52 +0900

    Remove the frame from SatelliteAssembly

--- src/features/SatelliteAssembly/index.tsx
+++ src/features/SatelliteAssembly/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useMemo } from "react";
 import styled from "styled-components";
 import { SIZE } from "../../data/SIZE";
-import { LectureFrame } from "../../elements/LectureFrame";
 import {
   BtnArea,
   ParameterArea,
@@ -11,14 +10,14 @@ import {
 import { SatelliteAssemblyProvider } from "./contexts";
 import { MasterData } from "./types";
 import { getMissionPartsIDs, handleMissionDataIDs } from "./utils";
-
 const ImageBackGround = new URL(
   "../../assets/prod/bg_grid.png",
   import.meta.url
 ).toString();
+
 const Main = styled.div`
-  height: ${SIZE.H};
-  width: ${SIZE.W};
+  height: ${SIZE.H}px;
+  width: ${SIZE.W}px;
   background-image: url(${ImageBackGround});
   display: flex;
   font-family: "UD デジタル 教科書体 N-B";
@@ -55,43 +54,35 @@ export const SatelliteAssembly: FC<SatelliteAssemblyProps> = ({
   const missionPartsIDs = getIDs(getMissionPartsIDs); // ミッションパーツ
 
   return (
-    <LectureFrame unitName="とりあえず仮" unitTitle="とりあえず仮">
-      <Main>
-        <SatelliteAssemblyProvider
-          initialValue={{
-            tabIndex: 0,
-            selectedMissionPartsIDs: [],
-            // 初期表示時はミッションパーツの最初のパーツを選択状態にする
-            selectedPartID: missionPartsIDs[0],
-            isPriceOver: false,
-            isMonthOver: false,
-            isLaunchOver: false,
-            isLoadingOver: false,
-            isWattsOver: false,
-            launchableMass: missionData.launchable_mass as
-              | "leo"
-              | "geo"
-              | "ooo",
-          }}
-        >
-          <PartsPreviewArea masterData={masterData} />
-          <div>
-            <div css={"margin-top: 18px"} />
-            <BtnArea />
-            <div css={"margin-top: 18px"} />
-            <ParameterArea
-              masterData={masterData}
-              maxBudget={missionData.max_budget}
-              preparationPeriod={missionData.preparation_period}
-            />
-            <div css={"margin-top: 18px"} />
-            <PartsSelectArea
-              missionData={missionData}
-              masterData={masterData}
-            />
-          </div>
-        </SatelliteAssemblyProvider>
-      </Main>
-    </LectureFrame>
+    <Main>
+      <SatelliteAssemblyProvider
+        initialValue={{
+          tabIndex: 0,
+          selectedMissionPartsIDs: [],
+          // 初期表示時はミッションパーツの最初のパーツを選択状態にする
+          selectedPartID: missionPartsIDs[0],
+          isPriceOver: false,
+          isMonthOver: false,
+          isLaunchOver: false,
+          isLoadingOver: false,
+          isWattsOver: false,
+          launchableMass: missionData.launchable_mass as "leo" | "geo" | "ooo",
+        }}
+      >
+        <PartsPreviewArea masterData={masterData} />
+        <div>
+          <div css={"margin-top: 18px"} />
+          <BtnArea />
+          <div css={"margin-top: 18px"} />
+          <ParameterArea
+            masterData={masterData}
+            maxBudget={missionData.max_budget}
+            preparationPeriod={missionData.preparation_period}
+          />
+          <div css={"margin-top: 18px"} />
+          <PartsSelectArea missionData={missionData} masterData={masterData} />
+        </div>
+      </SatelliteAssemblyProvider>
+    </Main>
   );
 };
