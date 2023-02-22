ca0e33b1d50e70047637192ea84b33d49e97a862
aks-tamura <tamura-junpei@akiba.kayac.studio>
2023-02-22 16:28:33 +0900

    データ確認の部分だけを表示するように修正

--- src/features/DisplayResult/index.tsx
+++ src/features/DisplayResult/index.tsx
@@ -1,16 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, useMemo } from "react";
 import styled from "styled-components";
-import { SIZE } from "../../data/SIZE";
-import { LectureFrame } from "../../elements/LectureFrame";
 import { ResultList } from "src-ibl-lecture-master-special/types";
-import { MissionPartsResults } from "./components/MissionPartsResults";
+import { ScrollArea } from "srn-ibl-component";
+import { pdfPath } from "../../data/pdfPath";
+import { OpenPdfBtn } from "../../elements/OpenPdfBtn";
+import { PartResultCard } from "../../elements/PartResultCard";
+import { TwoPartsResultCard } from "../../elements/TwoPartsResultCard";
+import { SAVED_PARTS } from "../../config";
 
 const Main = styled.div`
-  height: ${SIZE.H};
-  width: ${SIZE.W};
+  padding-right: 10px;
+  margin-left: 85px;
+  width: 1024px;
+  height: 608px;
+  font-size: 30px;
   font-family: "UD デジタル 教科書体 N-B";
-  padding-left: 38px;
-  padding-top: 25px;
+  line-height: 40px;
+  color: #5a5a5a;
+  text-align: center;
+  border: 3px solid #483bcb;
+  background-color: rgba(255, 255, 255, 0.5);
+`;
+const SContents = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 16px;
 `;
 
 const STitle = styled.div`
@@ -18,13 +33,14 @@ const STitle = styled.div`
   line-height: 38.58px;
   color: #5a5a5a;
 `;
-const SMessage = styled.div`
-  font-size: 30px;
-  line-height: 40px;
-  color: #5a5a5a;
-  text-align: center;
-  margin-top: 30px;
-`;
+
+export interface SavedParts {
+  missionPartsIDs: string[];
+  batteryID: string;
+  busID: string;
+  rocketID: string;
+}
+
 export interface DisplayResultProps {
   /**
    * マスターデータから注入されるJSONオブジェクト
@@ -36,21 +52,54 @@ export interface DisplayResultProps {
  * データ確認画面
  */
 export const DisplayResult: FC<DisplayResultProps> = ({ resultList }) => {
+  // 画面に表示するパーツ
+  const missionParts = useMemo(() => {
+    // ローカルストレージからパーツを取得
+    const item = localStorage.getItem(SAVED_PARTS);
+    const savedParts: SavedParts = item && JSON.parse(item);
+    // missionパーツ
+    const { missionPartsIDs } = savedParts;
+
+    // TODO:表示確認用 必要なくなったら消す
+    // const missionPartsIDs = ["4_2", "4_3", "4_6", "5_2", "7_1"];
+
+    return resultList.filter((result) => {
+      // part_aとpart_bに両方値がある場合
+      if (result.part_a && result.part_b) {
+        // part_aとpart_bの両方が保存したミッションパーツに含まれる場合をフィルタリング
+        return (
+          missionPartsIDs.includes(result.part_a ? result.part_a : "") &&
+          missionPartsIDs.includes(result.part_b ? result.part_b : "")
+        );
+      } else {
+        // missionPartがpart_a または part_bに含まれるものフィルタリング
+        return (
+          missionPartsIDs.includes(result.part_a ? result.part_a : "") ||
+          missionPartsIDs.includes(result.part_b ? result.part_b : "")
+        );
+      }
+    });
+  }, [resultList]);
+
   return (
-    <LectureFrame unitName="とりあえず仮" unitTitle="とりあえず仮">
-      <Main>
-        <STitle>■人工衛星が送ってきたデータを確認する</STitle>
-        <div css={"margin-top: 46px"} />
-        <MissionPartsResults {...{ resultList }} />
-        <div css={"margin-top: 30px"} />
-        <SMessage>
-          データを保存したら、このユニットは終了です。
-          <br />
-          次のユニットでは、このデータをもとに、
-          <br />
-          課題を解決する方法を考えるグループワークを行います。
-        </SMessage>
-      </Main>
-    </LectureFrame>
+    <Main>
+      <ScrollArea css={"height: 600px; "}>
+        <div css={"margin-top: 26px"} />
+        <STitle>人工衛星による成果を確認しよう</STitle>
+        <div css={"margin-top: 31px"} />
+        {missionParts.map((partsResult) => (
+          <SContents key={partsResult.result_id}>
+            {/* part_aとpart_bに両方値がある場合と片方しかない場合でコンポーネントを分ける */}
+            {partsResult.part_a && partsResult.part_b ? (
+              <TwoPartsResultCard resultList={partsResult} />
+            ) : (
+              <PartResultCard resultList={partsResult} />
+            )}
+            <div css={"margin-left: 24px"} />
+            <OpenPdfBtn filePath={pdfPath[partsResult.result_pdf]} />
+          </SContents>
+        ))}
+      </ScrollArea>
+    </Main>
   );
 };
