927f472a0e0260668b7139c94a5228208560891d
Mochan01 <ryohei.s@fantamstick.jp>
2023-03-01 23:08:35 +0900

    Append new custom hooks 'useGenerateDisableKey'

--- src/templates/Lecture/components/ControlPanel/ControlPanel.tsx
+++ src/templates/Lecture/components/ControlPanel/ControlPanel.tsx
@@ -1,24 +1,12 @@
-import React, { FC, useContext, useMemo } from "react";
-import styled from "styled-components";
+import React, { FC } from "react";
 import { SeekBar } from "../SeekBar";
 import { ControlButtons } from "../ControlButtons";
-import { Lecture } from "src-ibl-lecture-master-unit/types";
-import {
-  GlobalStateContext,
-  JsonDataProviderContext,
-} from "../../../../features/LectureRoot/providers";
-import {
-  handleJsonData,
-  getStepData,
-} from "../../../../features/LectureRoot/utils";
+import { useGenerateDisableKey } from "../../hooks";
 
 export interface ControlBarProps {
   onLectureLeave: (key: "begin" | "end") => void;
   className?: string;
 }
-
-/* lecture_panel.png */
-const Main = styled.div``;
 
 /**
  * コントロールパネル
@@ -27,33 +15,11 @@ export const ControlPanel: FC<ControlBarProps> = ({
   onLectureLeave,
   className,
 }) => {
-  const { progress } = useContext(GlobalStateContext);
-
-  const lectureData = useContext(JsonDataProviderContext) as Lecture[];
-  const getLectureData = handleJsonData(lectureData, progress);
-
-  const { special_lecture } = getLectureData(getStepData);
-
-  const disableKey = useMemo(() => {
-    // 衛星組み立て画面の場合
-    if (special_lecture.record_mission) {
-      return "some";
-    }
-    // 特別レクチャーの場合
-    if (
-      special_lecture.mission_select.length !== 0 ||
-      special_lecture.launch_key ||
-      special_lecture.display_result
-    ) {
-      return "full";
-    }
-    return "none";
-  }, [special_lecture]);
-
+  const disableKey = useGenerateDisableKey();
   return (
-    <Main {...{ className }}>
+    <div {...{ className }}>
       <SeekBar isActive={disableKey === "none"} />
       <ControlButtons disableKey={disableKey} {...{ onLectureLeave }} />
-    </Main>
+    </div>
   );
 };
