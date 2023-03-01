19fce244af132279eae664a79aad9e57ead00280
Mochan01 <ryohei.s@fantamstick.jp>
2023-03-01 21:39:21 +0900

    Make it able use mock for display result

--- src/features/Screen/utils/getPartsIDs.ts
+++ src/features/Screen/utils/getPartsIDs.ts
@@ -1,7 +1,14 @@
 import { SAVED_PARTS } from "../../../config";
 import { SavedParts } from "../../DisplayResult";
 
+const mockString =
+  // eslint-disable-next-line quotes
+  '{"missionPartsIDs":["4_2","4_3"],"rocketID":"1_5","busID":"2_4","batteryID":"3_2"}';
+
 export const getPartsIDs = (): SavedParts | undefined => {
-  const item = localStorage.getItem(SAVED_PARTS);
-  return item && JSON.parse(item);
+  const item =
+    process.env.NODE_ENV === "development"
+      ? mockString
+      : localStorage.getItem(SAVED_PARTS);
+  return item ? JSON.parse(item) : undefined;
 };
