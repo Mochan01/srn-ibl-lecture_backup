258d92ec39699db9d5524bd92ec4fe197c861aaa
aks-tamura <tamura-junpei@akiba.kayac.studio>
2023-03-06 16:41:57 +0900

    localStorageのmissionIDでフィルタリングするようには修正

--- src/features/Screen/utils/getPartsIDs.ts
+++ src/features/Screen/utils/getPartsIDs.ts
@@ -3,7 +3,7 @@ import { SavedParts } from "../../DisplayResult";
 
 const mockString =
   // eslint-disable-next-line quotes
-  '{"missionPartsIDs":["4_2","4_3"],"rocketID":"1_5","busID":"2_4","batteryID":"3_2"}';
+  '{"missionPartsIDs":["9_2","10_1"],"rocketID":"1_5","busID":"2_4","batteryID":"3_2"}';
 
 export const getPartsIDs = (): SavedParts | undefined => {
   const item =
@@ -11,4 +11,11 @@ export const getPartsIDs = (): SavedParts | undefined => {
       ? mockString
       : localStorage.getItem(SAVED_PARTS);
   return item ? JSON.parse(item) : undefined;
+};
+
+const mockMissionID = "mission_6";
+export const getSelectedMissionID = (): string | false | null => {
+  return process.env.NODE_ENV === "development"
+    ? mockMissionID
+    : typeof window !== "undefined" && localStorage.getItem("missionID");
 };
