412dfb153c375f488ce1f747683fbe50ecabd2da
aks-tamura <tamura-junpei@akiba.kayac.studio>
2023-03-09 20:49:45 +0900

    黒い帯が出ないように修正

--- src/elements/ScaleWrapper/index.tsx
+++ src/elements/ScaleWrapper/index.tsx
@@ -2,12 +2,19 @@ import React, { FC, ReactNode, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { useResizeWindow } from "./hooks";
 
+const ImgBg = new URL(
+  "../../assets/prod/lecture_bg.png",
+  import.meta.url
+).toString();
+
 export interface ScaleWrapperProps {
   children: ReactNode;
 }
 
 const Wrapper = styled.div`
-  background: #000;
+  background-image: url(${ImgBg});
+  background-size: cover;
+  background-repeat: no-repeat;
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -16,15 +23,14 @@ const Wrapper = styled.div`
   overflow: hidden;
 `;
 
-interface ContainerProps { $scale: number; };
-const Container = styled.div.attrs<ContainerProps>(
-  ({ $scale }) => ({ style: { transform: `scale(${ $scale })` } })
-)<ContainerProps>``;
+interface ContainerProps {
+  $scale: number;
+}
+const Container = styled.div.attrs<ContainerProps>(({ $scale }) => ({
+  style: { transform: `scale(${$scale})` },
+}))<ContainerProps>``;
 
-export const ScaleWrapper: FC<ScaleWrapperProps> = ({
-  children
-}) => {
-
+export const ScaleWrapper: FC<ScaleWrapperProps> = ({ children }) => {
   const [windowWidth, windowHeight] = useResizeWindow();
   const ref = useRef<HTMLDivElement>(null);
   const [$scale, setScale] = useState(1);
@@ -36,19 +42,17 @@ export const ScaleWrapper: FC<ScaleWrapperProps> = ({
     const windowRatio = windowHeight / windowWidth;
     const clientRatio = clientHeight / clientWidth;
 
-    const scale = windowRatio > clientRatio
-      ? windowWidth / clientWidth
-      : windowHeight / clientHeight;
+    const scale =
+      windowRatio > clientRatio
+        ? windowWidth / clientWidth
+        : windowHeight / clientHeight;
 
     setScale(Math.floor(scale * 1000) / 1000);
-
-  }, [windowWidth, windowHeight, ref.current]);
+  }, [windowWidth, windowHeight]);
 
   return (
     <Wrapper>
-      <Container { ...{ ref, $scale } }>
-        { children }
-      </Container>
+      <Container {...{ ref, $scale }}>{children}</Container>
     </Wrapper>
   );
 };
