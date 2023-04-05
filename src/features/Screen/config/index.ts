f527e2d14921368da71435edd3731e19282da40e
Mochan01 <ryohei.s@fantamstick.jp>
2023-04-05 18:49:59 +0900

    Update storybook of Screen component

---
+++ src/features/Screen/config/index.ts
@@ -0,0 +1,96 @@
+// mock for the storybook.
+export const screenData = [
+  {
+    progress: {
+      slide: 1,
+      step: 1,
+    },
+    images: {
+      display_files: [
+        "https://placehold.jp/30/657da6/ffffff/1286x890.png?text=画像1",
+        "https://placehold.jp/30/657da6/ffffff/1286x890.png?text=画像2",
+      ],
+      delete_files: [],
+    },
+    popup: {
+      popup_btns: [
+        "https://placehold.jp/30/fcba03/222222/300x80.png?text=ポップアップボタン1",
+      ],
+      popup_x: [100],
+      popup_y: [100],
+      display_popup: [
+        "https://placehold.jp/30/ff00ff/222222/500x500.png?text=ポップアップ1",
+      ],
+      popup_narration: ["quiz_correct.mp3"],
+    },
+    actions: {
+      action_btns: [],
+      action_x: [],
+      action_y: [],
+      action_goto: [],
+    },
+    question_select: {
+      question_x: 450,
+      question_y: 200,
+      button_1: "不正解",
+      ans_1: false,
+      button_2: "不正解",
+      ans_2: false,
+      button_3: "正解",
+      ans_3: true,
+      button_4: "不正解",
+      ans_4: false,
+    },
+    question_input: {
+      question_x: "",
+      question_y: "",
+      ans: "",
+    },
+    special_lecture: {
+      mission_select: ["ミッションID1", "ミッションID2"],
+      display_result: true,
+    },
+  },
+  {
+    progress: {
+      slide: 1,
+      step: 2,
+    },
+    images: {
+      display_files: [
+        "https://placehold.jp/30/77ff99/ffffff/300x300.png?text=画像1",
+      ],
+      delete_files: [],
+    },
+    popup: {
+      popup_btns: [],
+      popup_x: [300],
+      popup_y: [500],
+      display_popup: [ ],
+      popup_narration: [],
+    },
+    actions: {
+      action_btns: [],
+      action_x: [],
+      action_y: [],
+      action_goto: [],
+    },
+    question_select: {
+      question_x: 450,
+      question_y: 200,
+      ans_1: false,
+      ans_2: false,
+      ans_3: true,
+      ans_4: false,
+    },
+    question_input: {
+      question_x: "",
+      question_y: "",
+      ans: "",
+    },
+    special_lecture: {
+      mission_select: [],
+      display_result: true,
+    },
+  },
+] as const;
--- src/features/Screen/index.stories.tsx
+++ src/features/Screen/index.stories.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 import { Screen, ScreenProps } from ".";
+import { screenData } from "./config";
 
-import masterData from "../../assets/data/satellite_assembly_mock.json";
 export default {
   title: "features/Screen",
   component: Screen,
+  argTypes: {
+    step: {
+      control: { type: "number", min: 1, max: screenData.length, step: 1 },
+    },
+  },
 } as Meta;
 
 const template: Story<ScreenProps> = (args) => <Screen {...args} />;
-
-const resultList = masterData.result_list;
 
 export const sample = template.bind({});
 sample.args = {
@@ -19,111 +22,5 @@ sample.args = {
   actionGoTo: (actionGoto, missionID) =>
     console.log(`${actionGoto}に飛ぶ、ミッションID: ${missionID}`),
   onAnswer: (isAnswer) => console.log(isAnswer ? "正解" : "不正解"),
-  screenData: [
-    {
-      progress: {
-        slide: 1,
-        step: 1,
-      },
-      images: {
-        display_files: [
-          "https://placehold.jp/30/657da6/ffffff/1286x890.png?text=画像1",
-          "https://placehold.jp/30/657da6/ffffff/1286x890.png?text=画像2",
-        ],
-        delete_files: [],
-      },
-      popup: {
-        popup_btns: [
-          "https://placehold.jp/30/fcba03/222222/300x80.png?text=ポップアップボタン1",
-        ],
-        popup_x: [100],
-        popup_y: [100],
-        display_popup: [
-          "https://placehold.jp/30/ff00ff/222222/500x500.png?text=ポップアップ1",
-        ],
-        popup_narration: ["quiz_correct.mp3"],
-      },
-      actions: {
-        action_btns: [],
-        action_x: [],
-        action_y: [],
-        action_goto: [],
-      },
-      question_select: {
-        question_x: 450,
-        question_y: 200,
-        button_1: "不正解",
-        ans_1: false,
-        button_2: "不正解",
-        ans_2: false,
-        button_3: "正解",
-        ans_3: true,
-        button_4: "不正解",
-        ans_4: false,
-      },
-      question_input: {
-        question_x: "",
-        question_y: "",
-        ans: "",
-      },
-      special_lecture: {
-        mission_select: ["ミッションID1", "ミッションID2"],
-        display_result: true,
-      },
-    },
-    {
-      progress: {
-        slide: 1,
-        step: 2,
-      },
-      images: {
-        display_files: [],
-        delete_files: [
-          "https://placehold.jp/30/657da6/ffffff/1286x890.png?text=画像2",
-        ],
-      },
-      popup: {
-        popup_btns: [
-          "https://placehold.jp/30/fcba03/222222/300x80.png?text=ポップアップボタン2",
-        ],
-        popup_x: [500],
-        popup_y: [600],
-        display_popup: [
-          "https://placehold.jp/30/ff00ff/222222/500x500.png?text=ポップアップ2",
-        ],
-        popup_narration: ["quiz_correct.mp3"],
-      },
-      actions: {
-        action_btns: [
-          "https://placehold.jp/30/997799/222222/300x80.png?text=アクションボタン1",
-          "https://placehold.jp/30/997799/222222/300x80.png?text=アクションボタン2",
-        ],
-        action_x: [300, 600],
-        action_y: [300, 600],
-        action_goto: ["1_1", "2_1"],
-      },
-      special_lecture: {
-        mission_select: ["ミッションID1", "ミッションID2"],
-        launch_animation: "launch",
-      },
-      question_select: {
-        question_x: "",
-        question_y: "",
-        button_1: "",
-        ans_1: false,
-        button_2: "",
-        ans_2: false,
-        button_3: "",
-        ans_3: true,
-        button_4: "",
-        ans_4: false,
-      },
-      question_input: {
-        question_x: 450,
-        question_y: 200,
-        ans: "入力問題です",
-      },
-    },
-  ],
-  resultList: resultList,
+  screenData,
 };
