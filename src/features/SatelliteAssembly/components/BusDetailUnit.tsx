0df230e14fa3581baa617955190f7b24df3a94df
Mochan01 <ryohei.s@fantamstick.jp>
2023-04-11 00:14:49 +0900

    Fix the flg of render component

--- src/features/SatelliteAssembly/components/BusDetailUnit.tsx
+++ src/features/SatelliteAssembly/components/BusDetailUnit.tsx
@@ -7,15 +7,16 @@ import { SatelliteAssemblyStateContext } from "../contexts";
 import { SCategoryTitle, SCategoryDescription, STable } from "../styles";
 import { MasterData } from "../types";
 import { getCategoryDescription, getPartDetailData } from "../utils";
-const Main = styled.div`
-  padding-left: 20px;
-  padding-top: 12px;
-`;
 
 interface BusDetailUnitProps {
   masterData: MasterData;
   partsData: Bus[];
 }
+
+const Main = styled.div`
+  padding-left: 20px;
+  padding-top: 12px;
+`;
 
 export const BusDetailUnit: FC<BusDetailUnitProps> = ({
   masterData,
@@ -40,35 +41,35 @@ export const BusDetailUnit: FC<BusDetailUnitProps> = ({
       <PartDetail partName={partName} description={partDescription} />
       <div css={"margin-top: 7px"} />
       <STable>
-        {partDetail?.price_hundred_million && (
+        {!!partDetail?.price_hundred_million && (
           <PartCost
             cost_name={"価格（億円）"}
             cost={partDetail?.price_hundred_million}
             isCostOver={state.isPriceOver}
           />
         )}
-        {partDetail?.manufacturing_period_months && (
+        {!!partDetail?.manufacturing_period_months && (
           <PartCost
             cost_name={"製造期間（月）"}
             cost={partDetail?.manufacturing_period_months}
             isCostOver={state.isMonthOver}
           />
         )}
-        {partDetail?.max_loading_mass_kg && (
+        {!!partDetail?.max_loading_mass_kg && (
           <PartCost
             cost_name={"最大積載可能質量（kg）"}
             cost={partDetail?.max_loading_mass_kg}
             isCostOver={state.isLoadingOver}
           />
         )}
-        {partDetail?.body_mass_kg && (
+        {!!partDetail?.body_mass_kg && (
           <PartCost
             cost_name={"本体質量（kg）"}
             cost={partDetail?.body_mass_kg}
             isCostOver={state.isLaunchOver}
           />
         )}
-        {partDetail?.required_power_watts && (
+        {!!partDetail?.required_power_watts && (
           <PartCost
             cost_name={"衛星バス必要電力（W）"}
             cost={partDetail?.required_power_watts}
